21ebede00102d750433f0b76854456dbbe710fcc
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-06-15 21:39:51 +0900

    commit

--- apps/admin/models.py
+++ apps/admin/models.py
@@ -14,19 +14,19 @@ class Users(db.Model):
     # _password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)  # 用户邮箱不能为空，而且必须是唯一的
 
-    # #def __init__(self, username, password, email):
-    #     self.username = username
-    #     self.password = password
-    #     self.email = email
-    #
-    # @property
-    # def password(self):
-    #     return self._password
-    #
-    # @password.setter
-    # def password(self, raw_password):
-    #     self._password = generate_password_hash(raw_password)
-    #
-    # def check_password(self, raw_password):
-    #     result = check_password_hash(self.password, raw_password)  #
-    #     return result
+     def __init__(self, username, password, email):
+         self.username = username
+         self.password = password
+         self.email = email
+
+     @property
+     def password(self):
+         return self._password
+
+     @password.setter
+     def password(self, raw_password):
+         self._password = generate_password_hash(raw_password)
+
+     def check_password(self, raw_password):
+         result = check_password_hash(self.password, raw_password)  #
+         return result
--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for,jsonify
+from flask import Blueprint, render_template, request, session, redirect, url_for, jsonify
 from .models import Users
 from .forms import LoginForm
+from .decorators import login_required
+from io import BytesIO
 from flask import make_response
 from utils.captcha import create_validate_code
 from datetime import timedelta
@@ -67,34 +69,34 @@ def get_code():
     return response
 
 
-# @bp.route('/logout/')
-# @login_required
-# def logout():
-#    del session[user_id]
-#   return redorect(url_for('admin.login'))
+@bp.route('/logout/')
+@login_required
+def logout():
+    del session[user_id]
+    return redirect(url_for('admin.login'))
 
 
 # 个人信息页视图
 @bp.route('/profile/')
-# @login_required
+@login_required
 def profile():
     if config.ADMIN_USER_ID in session:
         user_id = session.get(config.ADMIN_USER_ID)
         user = Users.query.get(user_id)
-        return render_template('admin/profile.html', user=user)
+    return render_template('admin/profile.html', user=user)
 
 
-#核实校验密码
+# 核实校验密码
 @bp.route('/checkpwd/')
-#@login_required
+# @login_required
 def checkpwd():
     # user1 = request.args.get('username')
     oldpwd = request.args.get('oldpwd', '')
     # print(oldpwd)
-    if config.ADMIN_USER_ID  in session:
+    if config.ADMIN_USER_ID in session:
         user_id = session.get(config.ADMIN_USER_ID)
-        #user = Users.query.filter_by(username='admin').first()#这里作了修改
-        user = Users.query.filter(Users.uid==user_id).first()  # 这里作了修改
+        # user = Users.query.filter_by(username='admin').first()#这里作了修改
+        user = Users.query.filter(Users.uid == user_id).first()  # 这里作了修改
         if user.check_password(oldpwd):
             data = {
                 "name": user.email,
@@ -105,13 +107,13 @@ def checkpwd():
                 "name": None,
                 "status": 00
             }
-        return jsonify(data)
+    return jsonify(data)
 
 
-    @bp.route('/editpwd/',methods=['GET','POST'])
-    @login_required
-    defe editpwd():
-    if request.method =='GET':
+@bp.route('/editpwd/', methods=['GET', 'POST'])
+@login_required
+def editpwd():
+    if request.method == 'GET':
         return render_template('admin/edit_pwd.html')
     else:
         oldpwd = request.form.get('oldpwd')
@@ -122,4 +124,129 @@ def checkpwd():
         user = Users.query.filter_by(uid=user_id).first()
         user.password = newpwd1
         db.session.commit()
-        return render_template('admin/edit_pwd.html',message="密码修改成功")
+        return render_template('admin/edit_pwd.html', message="密码修改成功")
+
+
+def build_tree(data, p_id, level=0):
+    """
+    生成树菜单
+    :param data:    数据
+    :param p_id:    上级分类
+    :param level:   当前级别
+    :return:
+    """
+    tree = []
+    for row in data:
+        if row['parent_id'] == p_id:
+            row['level'] = level
+            child = build_tree(data, row['cat_id'], level + 1)
+            row['child'] = []
+            if child:
+                row['child'] += child
+            tree.append(row)
+
+    return tree
+
+
+# 生成分类
+def build_table(data, parent_title='顶级菜单'):
+    html = ''
+    for row in data:
+        splice = '├ '
+        cat_id = row['cat_id']
+        title = splice * row['level'] + row['cat_name']
+        tr_td = """<option value={cat_id}>  {title}</option>
+                                   """
+        if row['child']:
+            html += tr_td.format(class_name='top_menu', title=title, cat_id=cat_id)
+            html += build_table(row['child'], row['cat_name'])
+        else:
+            html += tr_td.format(class_name='', title=title, cat_id=cat_id)
+            # return html
+    return html
+
+
+# 添加分类
+@bp.route('/article_cat_add/', methods=['GET', 'POST'])
+@login_required
+def article_cat_add():
+    if request.method == 'GET':
+        categorys = Articles_Cat.query.all()  # 取得所有分类
+        list = []
+        data = {}
+
+        for cat in categorys:
+            data = dict(cat_id=cat.cat_id, parent_id=cat.parent_id, cat_name=cat.cat_name)
+            list.append(data)
+        data = build_tree(list, 0, 0)
+        print(data)
+        # print(list)
+        html = build_table(data, parent_title='顶级菜单')
+        # print(html)
+        return render_template('admin/article_cat.html', message=html)  # article_cat.html
+    else:
+        form = Article_cat(request.form)
+        p = Pinyin()
+        dir = request.form.get('dir')
+        print(dir)
+        if form.validate():
+            parent_id = request.form.get('parent_id')
+            cat_name = request.form.get('cat_name')
+            dir = request.form.get('dir')
+            check = request.form.get('check')
+            if check:
+                dir = request.form.get('cat_name')
+                dir = p.get_pinyin(dir, '')
+            else:
+                if dir:
+                    dir = request.form.get('dir')
+                else:
+                    dir = request.form.get('cat_name')
+                    dir = p.get_pinyin(dir, '')
+            keywords = request.form.get('keywords')
+            description = request.form.get('description')
+            cat_sort = request.form.get('cat_sort')
+            status = request.form.get('status')
+            insert = Articles_Cat(parent_id=parent_id, cat_name=cat_name, dir=dir, keywords=keywords,
+                                  description=description, cat_sort=cat_sort, status=status)
+            db.session.add(insert)
+            db.session.commit()
+            return redirect(url_for('admin.article_cat_list'))
+        else:
+            print("校验没有通过")
+            return "校验没通过 "
+
+
+# 栏目列表
+@bp.route('/article_cat_list/', methods=['GET'])
+@login_required
+@admin_auth
+def article_cat_list():
+    if request.method == 'GET':
+        categorys = Articles_Cat.query.all()  # 取得所有分类
+        list = []
+        data = {}
+
+        for cat in categorys:
+            data = dict(cat_id=cat.cat_id, parent_id=cat.parent_id, cat_name=cat.cat_name, description=cat.description,
+                        dir=cat.dir, cat_sort=cat.cat_sort)
+            list.append(data)
+        data = build_tree(list, 0, 0)
+        html = creat_cat_list(data, parent_title='顶级菜单')
+        return render_template('admin/articel_cat_list.html', message=html)
+
+
+@bp.route('/article_cat_edit/<id>/', methods=['GET', 'POST'])
+@login_required
+def article_cat_edit(id):
+    if request.method == 'GET':
+        cat_list = Articles_Cat.query.filter_by(cat_id=id).first()
+        categorys = Articles_Cat.query.all()  # 取得所有分类
+        list = []
+        data = {}
+        for cat in categorys:
+            data = dict(cat_id=cat.cat_id, parent_id=cat.parent_id, cat_name=cat.cat_name)
+            list.append(data)
+        data = build_tree(list, 0, 0)
+        html = build_table(data, parent_title='顶级菜单')
+        return render_template('admin/artical_cat_edit.html', content=cat_list, message=html)
